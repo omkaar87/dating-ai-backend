13e530c24ea06eb2026e5c71f5017039c54e58a6
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2024-08-17 22:23:31 +0530

    Initializer app in working state

---
+++ src/main/java/com/dating/dating_ai_backend/DatingAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dating.dating_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dating/dating_ai_backend/DatingAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dating.dating_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
