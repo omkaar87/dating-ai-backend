782d8aad189f40433bec56b117f3934afba84609
Omkar Shivadekar <omkarshivadekar.os@gmail.com>
2024-08-24 20:48:16 +0530

    Added cross-origin to enable endpoint access to front end app

--- src/main/java/com/dating/dating_ai_backend/profiles/ProfileController.java
+++ src/main/java/com/dating/dating_ai_backend/profiles/ProfileController.java
@@ -1,6 +1,7 @@
 package com.dating.dating_ai_backend.profiles;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ public class ProfileController {
 
     @Autowired
     private ProfileRepo profileRepo;
+    @CrossOrigin(origins = "*")
     @GetMapping("/profiles/random")
     public Profile getRandomProfile(){
         return profileRepo.getRandomProfile();
